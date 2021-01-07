63a44c573d0e807dc3ab5d726a9300c0196e4d28
Brian Abeyta-Pratt <babeytapratt@gmail.com>
2021-01-07 16:22:45 -0700

    feature/tasksBrian

--- database/seeds/04-admins.js
+++ database/seeds/04-admins.js
@@ -1,13 +1,8 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('admins').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('admins').insert([
-        {school_district: 'Half Hollow Hills', school_name: 'West', state: 'New York', country: 'USA', user_id: 1}, // 1
-        {school_district: 'Half Hollow Hills', school_name: 'East', state: 'New York', country: 'USA', user_id: 2}, // 2
-        {school_district: 'Suburban Bliss', school_name: 'North', state: 'Colorado', country: 'USA', user_id: 3} // 3
-      ]);
-    });
+  return knex('admins').insert([
+    {school_district: 'Half Hollow Hills', school_name: 'West', state: 'New York', country: 'USA', user_id: 1}, // 1
+    {school_district: 'Half Hollow Hills', school_name: 'East', state: 'New York', country: 'USA', user_id: 2}, // 2
+    {school_district: 'Suburban Bliss', school_name: 'North', state: 'Colorado', country: 'USA', user_id: 3} // 3
+  ]);
 };
--- database/seeds/05-mentors.js
+++ database/seeds/05-mentors.js
@@ -1,13 +1,9 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('mentors').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('mentors').insert([
-        {first_name: 'Charlie', last_name: 'Murphy', interests: 'Rick James concerts, comedy, & storytelling', career: 'comedian', state: 'New York', country: 'USA', preferred_times: 'After 8pm Eastern', user_id: 4, 'timezone': 4},
-        {first_name: 'John', last_name: 'Smith', interests: 'Math, Making Pizza, Motorcross', career: 'Accountant', state: 'California', country: 'USA', preferred_times: '5-7pm PST', user_id: 5, 'timezone': 1},
-        {first_name: 'Laura', last_name: 'Smith', interests: 'Skydiving', career: 'Accountant', state: 'California', country: 'USA', preferred_times: '5-7pm PST', user_id: 6, 'timezone': 1}
-      ]);
-    });
+  return knex('mentors').insert([
+    {first_name: 'Charlie', last_name: 'Murphy', interests: 'Rick James concerts, comedy, & storytelling', career: 'comedian', state: 'New York', country: 'USA', preferred_times: 'After 8pm Eastern', user_id: 4, 'timezone': 4},
+    {first_name: 'John', last_name: 'Smith', interests: 'Math, Making Pizza, Motorcross', career: 'Accountant', state: 'California', country: 'USA', preferred_times: '5-7pm PST', user_id: 5, 'timezone': 1},
+    {first_name: 'Laura', last_name: 'Smith', interests: 'Skydiving', career: 'Accountant', state: 'California', country: 'USA', preferred_times: '5-7pm PST', user_id: 6, 'timezone': 1},
+    {first_name: 'Jenny', last_name: 'Gump', interests: 'Running', career: 'Shrimp boat captain', state: 'Louisiana', country: 'USA', preferred_times: '1-3pm CST', user_id: 7, 'timezone': 3}
+  ]);
 };
